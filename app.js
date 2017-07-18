de62c01caec8fbf2dc0e14fd592bde05d8d6eebc
Jesuraj <Gnana.Jesuraj@agilysys.com>
2017-07-18 11:31:29 +0530

    init

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var PORT=8080;
+
+var io = require('socket.io')(server);
+
+
+io.on('connection', function (client) {
+
+    console.log('client connected');
+
+        client.on('join', function (nickName) {
+                console.log("user - ",nickName, "joined");
+
+                client.nickName = nickName;
+        });
+
+        client.on('messages', function (data) {
+                  client.broadcast.emit('messages', data);
+                  //saveMessages(data);
+        });
+
+         client.on('disconnect',function(){
+                  console.log("user -"+client.nickName+" left the chat");
+                  client.broadcast.emit('removechatter',client.nickName);
+
+
+          });
+
+});
+
+
+
+app.use(express.static('public'));
+app.use(express.static('src'));
+app.get('/', function (req, res) {
+
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+server.listen(PORT, function () {
+    console.log("server is running at port:"+PORT);
+
+})
