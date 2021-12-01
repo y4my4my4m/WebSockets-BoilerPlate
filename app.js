cfaf0fe481dc43a49c5d179fcbe507426003d04c
y4my4my4m <8145020+y4my4my4m@users.noreply.github.com>
2021-11-30 23:20:10 -0500

    test

--- app.js
+++ app.js
@@ -1,46 +1,104 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var PORT=8080;
+var PORT = 8080;
 
-var io = require('socket.io')(server);
+var io = require("socket.io")(server, {
+    transports: ['polling', 'websocket'],
+    upgradeTimeout: 100,
+    // allowRequest: (req,callback) => {
+    //     console.log(req);
+    // }
+});
 
 
-io.on('connection', function (client) {
+server.on('upgrade', function (req, socket, head) {
+    console.log(`UPGRADEEE!!!`);
+});
+server.on('upgrading', function (req, socket, head) {
+    console.log(`UPGRADEEE FAIL!!!`);
+});
 
-    console.log('client connected');
+server.on('ping', (ping) => {
+    console.log("serverping: ", ping);
+});
+server.on('pong', (pong) => {
+    console.log("serverpong: ", pong);
+});
+io.on('ping', (ping) => {
+    console.log("ping: ", ping);
+});
+io.on('pong', (pong) => {
+    console.log("pong: ", pong);
+});
+io.on('upgrade', () => {
+    // client.emit('upgrade', client.nickName)
+    console.log("!1111!upgrade")
+});
+io.on('connection', (client) => {
+    console.log('client connected, transport:', client.handshake.query.transport);
 
-        // client.on('join', function (nickName) {
-        //         console.log("user - ",nickName, "joined");
-        //
-        //         client.nickName = nickName;
-        // });
-        //
-        // client.on('messages', function (data) {
-        //           client.broadcast.emit('messages', data);
-        //           //saveMessages(data);
-        // });
-        //
-        //  client.on('disconnect',function(){
-        //           console.log("user -"+client.nickName+" left the chat");
-        //           client.broadcast.emit('removechatter',client.nickName);
-        //
-        //
-        //   });
+    client.on('join', (nickName) => {
+        console.log("user - ", nickName, "joined");
+        client.nickName = nickName;
+    });
+    client.on('error', (error) => {
+        console.log("error:", error);
+    });
+    client.on('upgrade', () => {
+        client.emit('upgrade', client.nickName)
+    });
 
+    client.on("connect_error", (err) => {
+        // revert to classic upgrade
+        // socket.io.opts.transports = ["polling", "websocket"];
+        client.emit('messages', err);
+        console.log(`CONNECT_ERROR: `, err);
+        console.log(`CONNECT_ERROR MSG: `, err.message);
+    });
+    client.on("reconnect_attempt", (err) => {
+        console.log(`reconnect_attempt: `, err);
+    });
+
+    client.on("reconnect", (err) => {
+        console.log(`reconnect: `, err);
+    });
+    client.on("reconnect_error", (err) => {
+        console.log(`reconnect_error: `, err);
+    });
+    client.on("reconnect_failed", (err) => {
+        console.log(`reconnect_failed: `, err);
+    });
+
+    client.on('messages', (data) => {
+        client.broadcast.emit('messages', data);
+        //saveMessages(data);
+    });
+
+    client.on('disconnect', (reason) => {
+        console.log("user -" + client.nickName + " left the chat");
+        client.broadcast.emit('removechatter', client.nickName);
+        // console.log("disconnect reason:", reason);
+    });
+
+    client.on('ping', (ping) => {
+        console.log("clientping: ", ping);
+    });
+    client.on('pong', (pong) => {
+        console.log("clientpong: ", pong);
+    });
 });
 
 
 
 app.use(express.static('public'));
 app.use(express.static('src'));
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 
     res.sendFile(__dirname + "/index.html");
 });
 
 
 server.listen(PORT, function () {
-    console.log("server is running at port:"+PORT);
-
+    console.log("server is running at port:" + PORT);
 })
--- src/js/angularApp.js
+++ src/js/angularApp.js
@@ -1,55 +1,104 @@
-var app=angular.module("chatapp",[]);
+var app = angular.module("chatapp", []);
 
-app.controller("chatController",function($scope){
+app.controller("chatController", function ($scope) {
 
-    $scope.chatMessages=[];
+  $scope.chatMessages = [];
 
-    $scope.userName=prompt("Enter your ChatName");
+  $scope.userName = prompt("Enter your ChatName");
 
-    var socket=io.connect('http://localhost:8080');
-    socket.on("connect",function(){
-      
-      //  socket.emit('join',$scope.userName);
+  var socket = io.connect('http://localhost:8080', {
+    transports: ['polling', 'websocket'],
+    upgrade: true
+  });
+  socket.on("connect", function () {
+
+    socket.emit('join', $scope.userName);
+    socket.on('ping', (ping) => {
+      console.log("socket ping: ", ping);
     });
-   //
-  //   socket.on("messages",function(data){
-  //   $scope.$apply(function(){
-  //       addChat(data,true);
-  //   });
-  //   });
-   //
-   //
-  //   socket.on('removechatter',function(name){
-  //       $scope.$apply(function(){
-  //         var messageObj={ username:'CHAT_BOT',chat:name+' left the chat...'};
-  //         addChat(messageObj,true);
-  //       });
-  //   });
-   //
-  //   $('#sendChat').click(function(){
-  //  // contruct the message object and emit the messages event to server
-  //   var chatText=$('#chatText').val();
-  //   var messageObj={ username:$scope.userName,chat:chatText}
-   //
-  //   socket.emit("messages",messageObj);
-  //       $scope.$apply(function(){
-  //          addChat(messageObj,false);
-  //       });
-  //   });
-   //
-   //
-  //   function addChat(msgObj,fromServer){
-  //       $scope.chatMessages.push(msgObj);
-  //       if(!fromServer){
-  //            $('#chatText').val('');
-  //       }
-   //
-   //
-  //   }
-   //
-  //   function removeChatters(name){
-   //
-  //       var messageObj={ username:'CHAT_BOT',chat:name+' left the chat...'};
-  //       addChat(messageObj,true);
-  //   }
+    socket.on('pong', (pong) => {
+        console.log("socket pong: ", pong);
+    });
+  });
+
+  socket.on("messages", function (data) {
+    $scope.$apply(function () {
+      addChat(data, true);
+    });
+  });
+
+
+  socket.on('upgrade', function (name) {
+    $scope.$apply(function () {
+      var messageObj = {
+        username: 'CHAT_BOT',
+        chat: `${name} upgraded`
+      };
+      console.log(`UPGRADED`);
+      addChat(messageObj, true);
+    });
+  });
+
+  socket.on("connect_error", (err) => {
+    // revert to classic upgrade
+    // socket.io.opts.transports = ["polling", "websocket"];
+    console.log(`CONNECT_ERROR: `, err);
+    console.log(`CONNECT_ERROR MSG: `, err.message);
+  });
+
+  socket.on("reconnect_error", (err) => {
+    console.log(`reconnect_error: `, err);
+  });
+  socket.on("reconnect_failed", (err) => {
+      console.log(`reconnect_failed: `, err);
+  });
+
+  socket.on("reconnect_attempt", (err) => {
+    console.log(`reconnect_attempt: `, err);
+  });
+
+  socket.on("reconnect", (err) => {
+    console.log(`reconnect: `, err);
+  });
+
+  socket.on('removechatter', function (name) {
+    $scope.$apply(function () {
+      var messageObj = {
+        username: 'CHAT_BOT',
+        chat: `${name} left the chat...`
+      };
+      addChat(messageObj, true);
+    });
+  });
+
+  $('#sendChat').click(function () {
+    // contruct the message object and emit the messages event to server
+    var chatText = $('#chatText').val();
+    var messageObj = {
+      username: $scope.userName,
+      chat: chatText
+    }
+
+    socket.emit("messages", messageObj);
+    $scope.$apply(function () {
+      addChat(messageObj, false);
+    });
+  });
+
+
+  function addChat(msgObj, fromServer) {
+    $scope.chatMessages.push(msgObj);
+    if (!fromServer) {
+      $('#chatText').val('');
+    }
+  }
+
+  function removeChatters(name) {
+
+    var messageObj = {
+      username: 'CHAT_BOT',
+      chat: name + ' left the chat...'
+    };
+    addChat(messageObj, true);
+  }
 });
